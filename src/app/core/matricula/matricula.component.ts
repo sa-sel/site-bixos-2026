69834bccec17eee83de7c07bfee964f193512acd
Fernando Cirilo Zanchetta <fe.cz@usp.br>
2026-01-14 15:42:18 -0300

    #5 - Update FUVEST calendar images and dates, journey guide link and improve some texts' grammar

--- src/app/core/matricula/matricula.component.ts
+++ src/app/core/matricula/matricula.component.ts
@@ -1,7 +1,25 @@
 import { Component } from '@angular/core'
+import { ImageGridItemModel } from '@models'
 
 @Component({
   selector: 'app-matricula',
   templateUrl: './matricula.component.html',
 })
-export class MatriculaComponent {}
+export class MatriculaComponent {
+  fuvestCalendar: ImageGridItemModel[] = [
+    {
+      image: {
+        alt: 'Calendário de Chamadas da FUVEST',
+        src: '/assets/images/matricula/calendariofuvestchamadas.jpg',
+      },
+      fill: true,
+    },
+    {
+      image: {
+        alt: 'Calendário de Listas de Espera da FUVEST',
+        src: '/assets/images/matricula/calendariofuvestesperas.jpg',
+      },
+      fill: true,
+    },
+  ]
+}
